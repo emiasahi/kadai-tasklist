c877dcda7d9ba9e6d12e613e7278bfb8535b28a5
nfnk <nefeneko@gmail.com>
2019-04-28 04:29:18 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
-  resources :tasks
+  #元の文章root to: 'tasks#index'
+  #元の文章resources :tasks
+  root to: 'toppages#index'
 end
